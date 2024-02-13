8aed4aa0aff0abec89cd10a44f21c584c1a85524
Patrickdjiz <djizpatrick@gmail.com>
2024-02-13 19:24:41 +1100

    x

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,10 +11,10 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/book/create' element={<CreateBooks />} />
-      <Route path='/book/details/:id' element={<ShowBook />} />
-      <Route path='/book/edit/:id' element={<EditBooks />} />
-      <Route path='/book/delete/:id' element={<DeleteBooks />} />
+      <Route path='/books/create' element={<CreateBooks />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBooks />} />
+      <Route path='/books/delete/:id' element={<DeleteBooks />} />
     </Routes>
   )
 }
